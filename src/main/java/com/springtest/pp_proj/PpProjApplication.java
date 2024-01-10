71f5a7498fe11597bad043b8cfd010b5c671041e
Egor Novikov <egornovikov8060@gmail.com>
2024-01-10 20:01:30 +0300

    Web-service layout

---
+++ src/main/java/com/springtest/pp_proj/PpProjApplication.java
@@ -0,0 +1,13 @@
+package com.springtest.pp_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpProjApplication.class, args);
+	}
+
+}
